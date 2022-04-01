2cf89149fdf0d0c4fe3f3b4006c3ee405ddbc888
Alex Gui <xnmalex@gmail.com>
2022-04-01 17:31:43 +0800

    set up application and API end point constant

--- app/src/main/java/com/alexgui/polecat/ApiEndPoint.kt
+++ app/src/main/java/com/alexgui/polecat/ApiEndPoint.kt
@@ -1,2 +1,7 @@
 package com.alexgui.polecat
 
+class ApiEndPoint{
+    companion object {
+        val BASE_URL = "https://cataas.com/";
+    }
+}
--- app/src/main/java/com/alexgui/polecat/CatFeedApplication.kt
+++ app/src/main/java/com/alexgui/polecat/CatFeedApplication.kt
@@ -1,2 +1,12 @@
 package com.alexgui.polecat
 
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CatFeedApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
